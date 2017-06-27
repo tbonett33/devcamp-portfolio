b054f545311de36350f9e27167cb34f645b570c9
Jordan Hudgens <jordan@devcamp.com>
2017-06-27 16:27:28 +0000

    fixed schema issue

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,28 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#10.times do |blog|
+#  Blog.create!(
+#      title: "My Blog Post #{blog}",
+#      body: "This is a bunch of random text"
+#    
+#    )
+#end
 #
-# Examples:
+#5.times do |skill|
+#  Skill.create!(
+#    title: "Rails #{skill}",
+#    percent_utilized: 15
+#    )
+#end
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#puts "5 skills created"
+#
+#
+#9.times do |portfolio_item|
+#    Portfolio.create!(
+#        title: "Portfolio title: #{portfolio_item}",
+#        subtitle: "My greate service",
+#        body: "a bunch of random text",
+#        main_image: ,
+#        thumb_image:
+#      )
+#end
+#puts "9 portfolio items created"
