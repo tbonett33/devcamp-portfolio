6fceed246a65469a18493f666bf18b0367cb545f
Jordan Hudgens <jordan@devcamp.com>
2017-06-28 14:52:27 +0000

    finally have the seeds file working again with the schema file

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,6 +11,9 @@ Blog.create!(
 )
 
 end
+
+puts "10 blog posts created"
+
 9.times do |portfolio_item|
   Portfolio.create!(
     title: "Portfolio title: #{portfolio_item}",
@@ -23,9 +26,14 @@ end
     thumb_image:"http://placehold.it/400x150"
     )
 end
+
+puts "9 portfolio items created"
+
 5.times do |skill|
   Skill.create!(
     title: "Rails #{skill}",
     percent_utilized: 15
   )
 end
+
+puts "5 skill items created"
