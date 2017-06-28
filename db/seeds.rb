d4e15e0414b822becc5287c805f8512d364be54b
Jordan Hudgens <jordan@devcamp.com>
2017-06-28 14:48:35 +0000

    ---

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,3 +11,21 @@ Blog.create!(
 )
 
 end
+9.times do |portfolio_item|
+  Portfolio.create!(
+    title: "Portfolio title: #{portfolio_item}",
+    subtitle: "Ruby on Rails",
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+    aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+    irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non 
+    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    main_image: "http://placehold.it/350x150" , 
+    thumb_image:"http://placehold.it/400x150"
+    )
+end
+5.times do |skill|
+  Skill.create!(
+    title: "Rails #{skill}",
+    percent_utilized: 15
+  )
+end
