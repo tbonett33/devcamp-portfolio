dc9dc53619f6f28b1686902be85618be4c67d6a6
Jordan Hudgens <jordan@devcamp.com>
2017-07-17 15:56:41 +0000

    integrate technologies model

--- db/seeds.rb
+++ db/seeds.rb
@@ -36,6 +36,12 @@ end
 end
 puts "9 portfolio items created"
 
+3.times do |technology|
+  Portfolio.last.technologies.create!(
+    name: "Technology #{technology}"
+  )
+  end
+puts "3 Technologies created"
 5.times do |skill|
   Skill.create!(
     title: "Rails #{skill}",
