4daf4a7a80285495b31febb815bcdaac95683613
Jordan Hudgens <jordan@devcamp.com>
2017-07-26 14:38:32 +0000

    hahahahaha

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,4 +1,6 @@
 class PortfoliosController < ApplicationController
+  layout 'portfolio'
+  
   def index
     @portfolio_items = Portfolio.all
   end
@@ -14,6 +16,7 @@ class PortfoliosController < ApplicationController
 
   def create
     @portfolio_item = Portfolio.new(portfolio_params)
+
     respond_to do |format|
       if @portfolio_item.save
         format.html { redirect_to portfolios_path, notice: 'Your portfolio item is now live.' }
@@ -25,6 +28,7 @@ class PortfoliosController < ApplicationController
 
   def edit
     @portfolio_item = Portfolio.find(params[:id])
+
   end
 
   def update
@@ -55,10 +59,15 @@ class PortfoliosController < ApplicationController
       format.html { redirect_to portfolios_url, notice: 'Record was removed.' }
     end
   end
-  
+
   private
-  
+
   def portfolio_params
-    params.require(:portfolio).permit(:title, :subtitle,:body, :image, technologies_attributes: [:name])
+    params.require(:portfolio).permit(:title,
+                                      :subtitle,
+                                      :body,
+                                      technologies_attributes: [:name]
+                                     )
   end
+
 end
