54f07a515c4891d25525476e3f8f271f998540fc
Jordan Hudgens <jordan@devcamp.com>
2017-08-02 17:17:46 +0000

    added jquery gem

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,11 +1,10 @@
-
 class PortfoliosController < ApplicationController
   before_action :set_portfolio_item, only: [:edit, :show, :update, :destroy]
   layout 'portfolio'
   access all: [:show, :index, :angular], user: {except: [:destroy, :new, :create, :update, :edit]}, site_admin: :all
   
   def index
-    @portfolio_items = Portfolio.by_position
+    @portfolio_items = Portfolio.all
   end
 
   def angular
